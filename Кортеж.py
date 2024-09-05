a1ddffe8bcc28dd939924e969f4b5c88f7d07167
andrey899 <andrey291281@mail.ru>
2024-09-05 23:26:06 +0500

    Add files via upload

---
+++ Кортеж.py
@@ -0,0 +1,8 @@
+immutable_var = 1, 2, 3, 4, 'aplle', 'стол'
+print(immutable_var)
+print(type(immutable_var))
+#immutable_var[0] = 9
+mutable_list = ([1, 2], 3, 4)
+print(mutable_list)
+mutable_list[0][1] = 7
+print(mutable_list)
